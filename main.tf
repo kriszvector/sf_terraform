00a60ab8df7fccb9bec54e08dc035ffc2c94068a
krisz <krisztian.z.szabo@gmail.com>
2026-06-01 14:00:28 +0200

    added new provider

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     snowflake = {
-      source  = "Snowflake-Labs/snowflake"
-      version = "1.0.5"
+      source  = "snowflakedb/snowflake"
+      version = "2.17.0"
     }
   }
 }
