4312d08795259fb1aaeb72499690b301a019ef87
krisz <krisztian.z.szabo@gmail.com>
2026-06-01 14:10:16 +0200

    added authenticator

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ provider "snowflake" {
   organization_name = "ZXYSDYJ" # required if not using profile. Can also be set via SNOWFLAKE_ORGANIZATION_NAME env var
   account_name      = "FB47703" # required if not using profile. Can also be set via SNOWFLAKE_ACCOUNT_NAME env var
   user              = "krisz"  # required if not using profile or token. Can also be set via SNOWFLAKE_USER env var
+  authenticator     = "SNOWFLAKE_JWT"
   private_key       = var.snowflake_private_key
 
   // optional
