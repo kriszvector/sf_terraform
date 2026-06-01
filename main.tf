f02db90f8abb460807f234cff8384ca4251ca617
krisz <krisztian.z.szabo@gmail.com>
2026-06-01 14:06:08 +0200

    added key

--- main.tf
+++ main.tf
@@ -11,11 +11,12 @@ provider "snowflake" {
   organization_name = "ZXYSDYJ" # required if not using profile. Can also be set via SNOWFLAKE_ORGANIZATION_NAME env var
   account_name      = "FB47703" # required if not using profile. Can also be set via SNOWFLAKE_ACCOUNT_NAME env var
   user              = "krisz"  # required if not using profile or token. Can also be set via SNOWFLAKE_USER env var
-  private_key = var.snowflake_private_key
+  private_key       = var.snowflake_private_key
 
   // optional
   role      = "ACCOUNTADMIN"
   warehouse = "COMPUTE_WH"
+
 }
 
 
